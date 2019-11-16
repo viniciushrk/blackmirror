327ed8a85d000143040c0338639b2d05dd74662c
vinicius <viniciusrosa444@gmail.com>
2019-11-16 03:32:27 -0400

    front

--- apiLaravelReact/routes/web.php
+++ apiLaravelReact/routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/teste', function () {
+    return view('teste');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/mensagem', function () {
+    return view('conversas.mensagem');
+});
+Route::get('/bar', function () {
+    return view('template.bar');
+});
+
+Route::get('/menu', function () {
+    return view('template.menu');
+});
